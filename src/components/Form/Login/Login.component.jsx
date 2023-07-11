10079cec8ec4f4ad81b16affc909037dc763cb84
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-07-11 07:59:39 -0300

    Ação de redirecionar para home

--- src/components/Form/Login/Login.component.jsx
+++ src/components/Form/Login/Login.component.jsx
@@ -1,7 +1,15 @@
+import { useNavigate } from "react-router-dom"
+
 export const FormLoginComponent = () => {
 
+    const navigate = useNavigate();
+
+    const redirectToLogin = () => {
+        navigate('/home')
+    }
+
     return (     
-        <form>
+        <form onSubmit={redirectToLogin}>
             <legend>Login</legend>
             <div className="input-group">
                 <label htmlFor="email">Email</label>
