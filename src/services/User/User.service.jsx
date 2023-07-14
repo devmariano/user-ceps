947a56885366ebbd7fbed0db012e58ae10eae84a
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-07-14 19:41:32 -0300

    criando server no db.json

--- src/services/User/User.service.jsx
+++ src/services/User/User.service.jsx
@@ -1,9 +1,9 @@
 import { LocalStorageService } from "./LocalStorage.service";
-
+//GET
 const Get = () => {
   return LocalStorageService.get('users');
 }
-
+//POST
 const Create = (data) => {
   const users = Get();
 
@@ -14,19 +14,19 @@ const Create = (data) => {
 
   LocalStorageService.set('users', [...users, data]);
 }
- 
+//GET 
 const Show = (id) => {
   return Get().find(user => user.id === id);
 }
-
+//GET
 const ShowByEmail = (email) => {
   return Get().find(user => user.email === email);
 }
-
+//DELETE
 const Delete = (id) => {
   LocalStorageService.set('users', Get().filter(user => user.id !== id));
 }
-
+//PUT/PATCH
 const Update = (id, data) => {
   const users = Get();
 
